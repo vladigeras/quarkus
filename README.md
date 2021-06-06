07ec36dd863bedf346e2c3e4eaf4f367075637a4
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2021-06-06 15:27:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+This project is a Quarkus framework demo (RESTApi)
---
+++ src/test/java/ru/vladigeras/quarkus/QuarkusAppTest.java
@@ -0,0 +1,13 @@
+package ru.vladigeras.quarkus;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class QuarkusAppTest {
+
+	@Test
+	public void test() {
+	}
+
+}
